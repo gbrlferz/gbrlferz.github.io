d502bdccd50c4786e8a9e20176c4b37e3f270070
gbrlferz <80206433+gbrlferz@users.noreply.github.com>
2023-10-26 02:05:05 -0300

    teste site

---
+++ data.js
@@ -0,0 +1,375 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rea-gourmet",
+      "name": "Área Gourmet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4517691582078331,
+          "pitch": 0.2566647071373396,
+          "rotation": 3.141592653589793,
+          "target": "1-rea-da-piscina-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-rea-da-piscina-1",
+      "name": "Área da Piscina-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.00800133480370846,
+          "pitch": 0.1862335377017459,
+          "rotation": 3.141592653589793,
+          "target": "0-rea-gourmet"
+        },
+        {
+          "yaw": -2.6806694955255104,
+          "pitch": 0.3495975651339567,
+          "rotation": 0.7853981633974483,
+          "target": "2-rea-da-piscina-2"
+        },
+        {
+          "yaw": -2.8702073715465595,
+          "pitch": 0.22333568397836245,
+          "rotation": 5.497787143782138,
+          "target": "3-rea-da-piscina-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-rea-da-piscina-2",
+      "name": "Área da Piscina-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07436799814841599,
+          "pitch": 0.5128153698171865,
+          "rotation": 5.497787143782138,
+          "target": "3-rea-da-piscina-3"
+        },
+        {
+          "yaw": -2.7791371291249174,
+          "pitch": 0.313116564760632,
+          "rotation": 0,
+          "target": "1-rea-da-piscina-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-rea-da-piscina-3",
+      "name": "Área da Piscina-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21080397377633098,
+          "pitch": 0.3168226432908767,
+          "rotation": 0.7853981633974483,
+          "target": "4-rea-da-piscina-4"
+        },
+        {
+          "yaw": -2.163261807899694,
+          "pitch": 0.4505153206908865,
+          "rotation": 5.497787143782138,
+          "target": "2-rea-da-piscina-2"
+        },
+        {
+          "yaw": -1.9520402727572144,
+          "pitch": 0.21835540001356435,
+          "rotation": 0,
+          "target": "1-rea-da-piscina-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-rea-da-piscina-4",
+      "name": "Área da Piscina-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6070718668417907,
+          "pitch": 0.2619363888915007,
+          "rotation": 0,
+          "target": "5-garagem"
+        },
+        {
+          "yaw": 0.9211292875856429,
+          "pitch": 0.10821402673721181,
+          "rotation": 0.7853981633974483,
+          "target": "6-frente"
+        },
+        {
+          "yaw": 3.0234883441784843,
+          "pitch": 0.31499193459992547,
+          "rotation": 0.7853981633974483,
+          "target": "3-rea-da-piscina-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-garagem",
+      "name": "Garagem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3069841999278555,
+          "pitch": 0.3625292726977598,
+          "rotation": 0,
+          "target": "6-frente"
+        },
+        {
+          "yaw": -0.7693228626021362,
+          "pitch": 0.264165233522494,
+          "rotation": 5.497787143782138,
+          "target": "4-rea-da-piscina-4"
+        },
+        {
+          "yaw": -1.174933837146499,
+          "pitch": 0.15568928985599761,
+          "rotation": 0.7853981633974483,
+          "target": "3-rea-da-piscina-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-frente",
+      "name": "Frente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.298058254824813,
+          "pitch": -0.5552532241760986,
+          "rotation": 0,
+          "target": "7-frente-noite"
+        },
+        {
+          "yaw": -0.8380202051152796,
+          "pitch": 0.22262560991596203,
+          "rotation": 7.0685834705770345,
+          "target": "5-garagem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-frente-noite",
+      "name": "Frente-Noite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3937896368843674,
+          "pitch": -0.5127067519731678,
+          "rotation": 0,
+          "target": "6-frente"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Projeto Oseas e Penha",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
